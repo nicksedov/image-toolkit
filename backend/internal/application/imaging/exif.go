cb363db445af2c174a762a834b2839cd33355114
Николай Седов <nasedov@gmail.com>
2026-05-07 02:38:24 +0300

    exif

--- backend/internal/application/imaging/exif.go
+++ backend/internal/application/imaging/exif.go
@@ -15,8 +15,8 @@ import (
 	"strings"
 	"time"
 
-	_ "github.com/disintegration/imaging"
 	"github.com/barasher/go-exiftool"
+	_ "github.com/disintegration/imaging"
 	_ "golang.org/x/image/bmp"
 	_ "golang.org/x/image/tiff"
 	_ "golang.org/x/image/webp"
